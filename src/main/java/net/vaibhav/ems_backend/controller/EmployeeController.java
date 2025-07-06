58bd321e424679e965e0bb65a2cb905bcf96eead
Vaibhav <vaibhavtiwary7766@gmail.com>
2025-07-06 13:40:41 +0530

    create add employee Rest api

---
+++ src/main/java/net/vaibhav/ems_backend/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package net.vaibhav.ems_backend.controller;
+
+
+import net.vaibhav.ems_backend.dto.EmployeeDto;
+import net.vaibhav.ems_backend.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    //Build Add Employee REST API
+
+    @PostMapping
+    public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+}
